906b8f84c74cec6a7343353f266e348eccc9a9b9
sirolf2009 <masterflappie@gmail.com>
2019-05-23 11:06:36 +0200

    cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,27 +1,31 @@
-set -gx PATH ~/bin $PATH
-set -gx PATH /opt/prtg/bin $PATH
 set -gx GOPATH /opt/go
-set -gx PATH /opt/go/bin $PATH
-set -gx PATH /opt/rhyme $PATH
 set -gx default_user floris
 set -gx SHELL /usr/bin/fish
 set scrapers 50hertz amprion apg apx britned casc ceps deutscheboerse drax eex-ftp eex elexon elia energieopwek engie entsoe epex exaa gemini generic-ftp generic-html gme gpi iesoe intradaycapacity kmw marke montel nationalgrid netztransparenz nordpoolspot northpool omie ree regelleistung rheinenergie rte sheffielduni swissgrid tennet transnet unicorn weeronline windstats xrate
 set cassandra_test_servers cassandratest1 cassandratest2
 set cassandra_prod_servers cassandra-serv1 cassandra-serv2 cassandra-serv3 cassandra-serv4 cassandra-serv5 cassandra-serv6
 
-# Load fishmarks (http://github.com/techwizrd/fishmarks)
-. $HOME/.fishmarks/marks.fish
-
 set -x MSSQL_CLI_TELEMETRY_OPTOUT 1
 set -gx TTC_REPOS /home/floris/git
 set -gx TTC_WEATHER Rotterdam
 set -gx TTC_APIKEYS false
 
+alias mci 'mvn clean install'
+alias mcia 'mvn clean install appassembler:assemble'
+
 if [ (hostname) = "northpool2" ]
+  set -gx PATH ~/bin $PATH
+  set -gx PATH /opt/prtg/bin $PATH
+  set -gx PATH /opt/go/bin $PATH
+  set -gx PATH /opt/rhyme $PATH
+
   alias cassandra-common 'cd ~/git/cassandra-common'
   alias common 'cd ~/git/cassandra-common'
   alias cassandra-service 'cd ~/git/cassandra-service'
   alias generic-scraper 'cd ~/git/generic-scraper'
+  alias util 'cd ~/git/util'
+  alias ftp-puller 'cd ~/git/ftp-puller'
+  alias trading-platform 'cd ~/git/TradingPlatform'
 
   alias 50hertz 'cd ~/git/50hertz-scraper'
   alias amprion 'cd ~/git/amprion-scraper'
